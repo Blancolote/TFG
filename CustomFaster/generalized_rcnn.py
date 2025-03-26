5be117c43a28278ece382905536840cd5993a0c1
Samuel <slbl2@gcloud.ua.es>
2025-03-26 17:18:06 +0100

    Se sigue

--- CustomFaster/generalized_rcnn.py
+++ CustomFaster/generalized_rcnn.py
@@ -112,7 +112,7 @@ class GeneralizedRCNN(nn.Module):
         if isinstance(features, torch.Tensor):
             features = OrderedDict([("0", features)])
         proposals, proposal_losses = self.rpn(new_images, features, targets)
-        detections, detector_losses = self.roi_heads(features, proposals, new_images.size(), targets)
+        detections, detector_losses = self.roi_heads(features, proposals, new_images.image_sizes, targets)
         
         #bloque modificado--> no se hace postprocess aquí porque las imágenes ya está redimensionadas
         #detections = self.transform.postprocess(detections, new_images.size(), original_image_sizes)  # type: ignore[operator]
